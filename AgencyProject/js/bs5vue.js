e2e8678922475892b9999c9291e9e1d16a25ce7f
Llamahere <aa99919@gmail.com>
2024-11-08 11:57:36 +0800

    20241108

---
+++ AgencyProject/js/bs5vue.js
@@ -0,0 +1,28 @@
+const {createApp,ref}  = Vue;
+
+createApp({
+    data() {
+        return{
+            Services:[
+                {icon: "fa-shopping-cart", heading:"E-Commerce", text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."},
+                {icon: "fa-laptop", heading:"Responsive Design", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."},
+                {icon: "fa-lock", heading:"Web Security", text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."}
+            ]
+        }
+    }
+}).mount("#services");
+
+createApp({
+    data() {
+        return{
+            Portfolio:[
+                {heading:"Round Icons",photo:"", text:"Graphic Design",icon:"fa-plus"},
+                {heading:"Startup Framework",photo:"",text:"Website Design",icon:"fa-plus"},
+                {heading:"Treehouse",photo:"", text:"Website Design",icon:"fa-plus"},
+                {heading:"Golden",photo:"", text:"Website Design",icon:"fa-plus"},
+                {heading:"Escape",photo:"", text:"Website Design",icon:"fa-plus"},
+                {heading:"Dreams",photo:"", text:"Website Design",icon:"fa-plus"}
+            ]
+        }
+    }
+}).mount("#portfolio")
