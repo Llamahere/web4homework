352dab19eddb04a3cd91c5ae669ad8214f207631
Llamahere <145099642+Llamahere@users.noreply.github.com>
2024-12-19 13:12:05 +0800

    20241219

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var DB = require("nedb-promises");
 var ProfolioDB = DB.create(__dirname+"/profolio.db");
 var ContactDB = DB.create(__dirname+"/contact.db");
  
- ProfolioDB.insert([
+/* ProfolioDB.insert([
      { modal: "#portfolioModal1", imgSrc: "modalroundicons.png", heading: "Round Icons", text: "Graphic Design" },
      { modal: "#portfolioModal2", imgSrc: "startup-framework.png", heading: "Startup Framework", text: "Website Design" },
      { modal: "#portfolioModal3", imgSrc: "treehouse.png", heading: "Treehouse", text: "Website Design" },
@@ -46,7 +46,7 @@ server.get("/profolio", (req,res)=>{
 server.post("/contact_me", (req,res)=>{
      ContactDB.insert(req.body);
      res.send("OK");
-})
+})*/
 
 server.listen(80, ()=>{
     console.log("Server is running at port 80.");
